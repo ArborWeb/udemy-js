c636965cb635eac9ad0c23885a82d84f4a79cabd
Guilherme Weissheimer <weissheimerguilherme@lessentech.com>
2020-01-01 20:22:46 -0300

    64.N05

---
+++ node/index.js
@@ -0,0 +1,43 @@
+const http = require("http");
+
+let server = http.createServer((req, res) => {
+
+	console.log('URL', req.url);
+
+	console.log('METHOD', req.method);
+
+	switch (req.url) {
+		case "/":
+
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "text/html");
+			res.end("<h1>Olá mundo!</h1>")
+
+			break;
+
+		case "/user":
+
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "application/json");
+			res.end(JSON.stringify({
+
+				users: [{
+
+					name: "Guilherme",
+					age: 27
+
+				}]
+
+			})); 
+
+			break;
+
+	}
+
+});
+
+server.listen(3000, "127.0.0.1", () => {
+
+	console.log('servidor rodando');
+
+});
